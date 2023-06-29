63af51031c418d46cf05ad1214199611f9dd257b
Shreyas Shetty <109335377+shreyasshetty1468@users.noreply.github.com>
2023-06-29 13:02:12 +0530

    Testing

--- src/main/java/com/project1/project1/Spring01Class.java
+++ src/main/java/com/project1/project1/Spring01Class.java
@@ -7,19 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 @Configuration
-@ComponentScan("com.project1.project1.game")
 public class Spring01Class {
-    @Bean
-    public GamingInterface game() {
-        return new CricketGame();
-    }
+
     @Bean
     public GameRunner gameRunner(GamingInterface game) {
         return new GameRunner(game);
     }
+
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(Spring01Class.class);
-        context.getBean("game", GamingInterface.class).up();
         context.getBean("gameRunner", GameRunner.class).run();
     }
 }
